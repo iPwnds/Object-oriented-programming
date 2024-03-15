048e4f37ed8489c34389de2780742ea141d58f60
Florian Braùn <florian.braun@student.kuleuven.be>
2024-03-15 11:30:47 +0000

    Revert "small changes"
    This reverts commit 368b1894df5d33406a8d03abf6c130b0836518ea

--- src/main/java/sim1/Simulation.java
+++ src/main/java/sim1/Simulation.java
@@ -154,8 +154,7 @@ public class Simulation
  	 * @return | result >= 0
  	 * @post | result <= world.getPopulationA().length
      */
-    private int countSurvivingCreatureB() 
-    {
+    private int countSurvivingCreatureB() {
     	int count = 0;
         CreatureA[] populationA = world.getPopulationA();
         
@@ -238,6 +237,9 @@ public class Simulation
      * 
      * @post | result != null
      * @post | result.length == count
+     * @post | Arrays.stream(result).allMatch(c -> c != null)
+     * @post | Arrays.stream(result).allMatch(c -> parentGeneration.contains(c))
+     * @post | Arrays.stream(result).allMatch(c -> RandomUtil.integer(100) < Constants.MUT_RATE || c.isEqual(parentGeneration.get(0)))
      */
     private Chromosome[] computeOffspring(ArrayList<Chromosome> parentGeneration, int count)
     {
