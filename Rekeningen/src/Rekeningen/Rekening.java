f92c684725bdb1e1908d4e3f64efdbfd7795c6eb
Florian Braun <florianb.priv@gmail.com>
2024-04-04 21:10:37 +0200

    adding final rekening.java

--- Rekeningen/src/Rekeningen/Rekening.java
+++ Rekeningen/src/Rekeningen/Rekening.java
@@ -1,5 +1,49 @@
 package Rekeningen;
 
-public class Rekening {
+public abstract class Rekening {
+	
+	private int balans;
+	
+	/**
+	 * @basic
+	 */
+	public int getBalans() { return balans; }
+	
+	/**
+	 * @post | getBalans() == 0
+	 */
+	protected Rekening() {}
+	
+	/**
+	 * @mutates | this
+	 * @post | getBalans() == nieuweBalans
+	 */
+	protected void setBalans(int nieuweBalans) {
+		this.balans = nieuweBalans;
+	}
+	
+	/**
+	 * @pre | 0 <= bedrag
+	 * @mutates | this
+	 * @post | getBalans() == old(getBalans()) + bedrag
+	 */
+	public void stort(int bedrag) {
+		balans += bedrag;
+	}
+	
+	/**
+	 * @pre | 0 <= bedrag
+	 * @mutates | this
+	 * @post | 0 <= result
+	 * @post | result <= bedrag
+	 * @post | getBalans() == old(getBalans()) - result
+	 */
+	public abstract int neemAf(int bedrag);
+	
+	/**
+	 * @pre | andere != null
+	 * @inspects | this, andere
+	 */
+	public abstract boolean isInDezelfdeToestandAls(Rekening andere);
 
 }
