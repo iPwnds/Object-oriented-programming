606b14a939a0a721d750691c806aee193d96a331
Florian Braun <florianb.priv@gmail.com>
2024-04-18 13:43:15 +0200

    final Behavior/immobilebehavior

--- src/main/java/sim/behaviors/Behavior.java
+++ src/main/java/sim/behaviors/Behavior.java
@@ -7,67 +7,56 @@ import util.Color;
 
 /**
  * @immutable
- * This class represents the behavior of a creature in the simulation.
+ * 
+ * Represents a behavior that can be applied to a creature in a simulated world.
  */
-public abstract class Behavior
-{
-	public Chromosome chromosome;
-	
+public abstract class Behavior {
+
+    private Chromosome chromosome;
+
     /**
-     * Constructs a new Behavior with the specified chromosome.
-     * @param chromosome The chromosome associated with this behavior.
+     * Initializes a behavior with the specified chromosome.
+     * 
+     * @param chromosome - The chromosome representing the parameters of the behavior.
      * @pre | chromosome != null
-     * @post | this.chromosome == null
      */
-	public Behavior(Chromosome chromosome)
-	{
-		this.chromosome = null;
-	}
-	
+    public Behavior(Chromosome chromosome) {
+        this.chromosome = chromosome;
+    }
+
     /**
-     * Returns the chromosome associated with this behavior.
-     * @return | The chromosome associated with this behavior.
-     * @post | result == this.chromosome
+     * Retrieves the chromosome associated with this behavior.
+     * 
+     * @return The chromosome representing the parameters of the behavior.
      */
-    public Chromosome getChromosome()
-    {
+    public Chromosome getChromosome() {
         return chromosome;
     }
-	
+
     /**
-     * Applies this behavior to the specified creature in the specified world.
-     * @param world The world in which the creature exists.
-     * @param creature The creature to which this behavior is applied.
-     * @pre | world != null && creature != null
+     * Applies the behavior to the specified creature in the given world.
+     * 
+     * @param world - The world where the creature resides.
+     * @param creature - The creature to which the behavior will be applied.
+     * @pre | world != null
+     * @pre | creature != null
      */
     public abstract void applyBehavior(World world, Creature creature);
-    
+
     /**
      * LEGIT
      */
     public Color getColor() {
     	return Color.BLACK;
     }
-    
+
     /**
-     * Returns a copy of this behavior with the specified chromosome.
-     * @param chromosome The chromosome for the copied behavior.
-     * @return A copy of this behavior with the specified chromosome.
+     * Creates a copy of this behavior with the specified chromosome.
+     * 
+     * @param chromosome - The chromosome for the copied behavior.
+     * @return A new instance of the behavior with the specified chromosome.
      * @pre | chromosome != null
-     * @post | result != null && result.getChromosome() == chromosome
+     * @creates | result
      */
-    public Behavior copyWithChromosome(Chromosome chromosome) {
-        // Create a new instance of the behavior class
-        Behavior copy = null;
-        try {
-            copy = getClass().newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        
-        // Set the chromosome of the copy
-        copy.chromosome = chromosome;
-        
-        return copy;
-    }
+    public abstract Behavior copyWithChromosome(Chromosome chromosome);
 }
--- src/main/java/sim/behaviors/ImmobileBehavior.java
+++ src/main/java/sim/behaviors/ImmobileBehavior.java
@@ -5,30 +5,55 @@ import sim.Creature;
 import sim.World;
 import util.Color;
 
-
 /**
- * @invar | getColor() .equals( Color.WHITE )
+ * Represents a behavior where the creature remains immobile.
  * 
+ * @invar | getColor().equals(Color.WHITE)
  */
-public class ImmobileBehavior extends Behavior
-{
-	public ImmobileBehavior(Chromosome chromosome)
-	{
-		super(chromosome);
-	}
-	
-    @Override
-    public void applyBehavior(World world, Creature creature)
-    {
-        // NOP
+public class ImmobileBehavior extends Behavior {
+    
+    /**
+     * Initializes an immobile behavior with the given chromosome.
+     * 
+     * @param chromosome - The chromosome representing the behavior parameters.
+     * @pre | chromosome != null
+     */
+    public ImmobileBehavior(Chromosome chromosome) {
+        super(chromosome);
     }
     
-
-    
-
+    /**
+     * Applies the immobile behavior to the creature in the world.
+     * 
+     * @param world - The world where the creature resides.
+     * @param creature - The creature to apply the behavior to.
+     * @pre | world != null
+     * @pre | creature != null
+     */
     @Override
-    public ImmobileBehavior copyWithChromosome(Chromosome chromosome)
-    {
-    	return new ImmobileBehavior(chromosome);
+    public void applyBehavior(World world, Creature creature) {
+        // NOP (No Operation) - The creature remains immobile.
+    }
+    
+    /**
+     * Creates a copy of the immobile behavior with the given chromosome.
+     * 
+     * @param chromosome - The chromosome for the copied behavior.
+     * @return A new instance of ImmobileBehavior with the specified chromosome.
+     * @pre | chromosome != null
+     */
+    @Override
+    public ImmobileBehavior copyWithChromosome(Chromosome chromosome) {
+        return new ImmobileBehavior(chromosome);
+    }
+    
+    /**
+     * Gets the color of the immobile creature.
+     * 
+     * @return The color of the immobile creature, which is always white.
+     */
+    @Override
+    public Color getColor() {
+        return Color.WHITE;
     }
 }
