3fe293b8143757d49de31c70c21d6a55471f8f13
Florian Braun <florianb.priv@gmail.com>
2024-04-19 15:24:32 +0200

    final final

---
+++ src/test/java/StudentTests/SensorNeuronTest.java
@@ -0,0 +1,14 @@
+package StudentTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SensorNeuronTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
