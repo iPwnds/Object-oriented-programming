2e21d3f857b6987d8b2ef07789a8b32961f3514d
Florian Braun <florianb.priv@gmail.com>
2024-04-28 21:41:29 +0200

    adding team and student

--- Bigteams/src/Bigteams/Team.java
+++ Bigteams/src/Bigteams/Team.java
@@ -1,5 +1,36 @@
 package Bigteams;
 
-public class Team {
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * Each instance of this class represents a team
+ * in a team membership graph.
+ * 
+ * @invar | getMembers().stream().allMatch(s -> s != null && s.getTeam() == this)
+ */
+public class Team {
+	
+	/**
+	 * @invar | members != null
+	 * @invar | members.stream().allMatch(m ->
+	 *        |     m != null && m.team == this)
+	 *        
+	 * @representationObject
+	 * @peerObjects
+	 */
+	Set<Student> members = new HashSet<>();
+	
+	/**
+	 * @post | result != null
+	 *
+	 * @creates | result
+	 * @peerObjects
+	 */
+	public Set<Student> getMembers() { return Set.copyOf(members); }
+
+	/**
+	 * @post | getMembers().isEmpty()
+	 */
+	public Team() {}
 }
