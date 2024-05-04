1f28da4e2c18222b3cf4d91cb535a6e51637b405
Florian Braun <florianb.priv@gmail.com>
2024-05-04 23:44:20 +0200

    final commit

--- ExamensEnLokalen/src/ExamensEnLokalen/Examen.java
+++ ExamensEnLokalen/src/ExamensEnLokalen/Examen.java
@@ -1,5 +1,54 @@
 package ExamensEnLokalen;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Each instance of this class represents a exam
+ * in an exams-and-rooms graph.
+ * 
+ * @invar | getRooms().stream().allMatch(r -> r.getExams().contains(this))
+ */
 public class Examen {
+	
+	/**
+	 * @invar | rooms != null
+	 * @invar | rooms.stream().allMatch(r -> r != null)
+	 * @invar | rooms.stream().allMatch(r -> r.exams.contains(this))
+	 * @representationObject
+	 * @peerObjects
+	 */
+	HashSet<Lokaal> rooms = new HashSet<>();
+	
+	/**
+	 * @post | result != null
+	 * @creates | result
+	 * @post | result.stream().allMatch(r -> r != null)
+	 * @peerObjects
+	 */
+	public Set<Lokaal> getRooms() { return Set.copyOf(rooms); }
+	
+	/**
+	 * @post | getRooms().isEmpty()
+	 */
+	public Examen() {}
+	
+	/**
+	 * @pre | room != null
+	 * @mutates_properties | getRooms(), room.getExams()
+	 */
+	public void addRoom(Lokaal room) {
+		rooms.add(room);
+		room.exams.add(this);
+	}
+	
+	/**
+	 * @pre | room != null
+	 * @mutates_properties | getRooms(), room.getExams()
+	 */
+	public void removeRoom(Lokaal room) {
+		rooms.remove(room);
+		room.exams.remove(this);
+	}
 
 }
