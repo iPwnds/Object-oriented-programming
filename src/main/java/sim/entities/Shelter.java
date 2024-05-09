4272f994f3f14b6c3578a89c1aa4b6826433f323
Florian Braun <florianb.priv@gmail.com>
2024-05-09 03:25:00 +0200

    shelter documentation and implementation

--- src/main/java/sim/entities/Shelter.java
+++ src/main/java/sim/entities/Shelter.java
@@ -1,66 +1,159 @@
 package sim.entities;
 
 import java.util.ArrayList;
-
 import sim.Constants;
 import util.Color;
 import util.Logic;
 import util.Orientation;
 import util.Point;
 import util.RandomUtil;
+import util.Vector;
 
+/**
+ * The Shelter class represents a shelter in the simulation, acting as a home for preys.
+ * It extends the MortalEntity class.
+ */
 public class Shelter extends MortalEntity
 {
     /**
+     * The list of preys inhabiting the shelter.
+     * 
 	 * @peerObjects
 	 * @representationObject
      */
     final ArrayList<Prey> inhabitants;
 
-    
+    /**
+     * Creates a new Shelter object with the specified parameters.
+     * 
+     * @param world The world in which the shelter exists.
+     * @param position The initial position of the shelter.
+     * @param orientation The initial orientation of the shelter.
+     * 
+     * @throws IllegalArgumentException if any of the parameters are null.
+     * 
+     * @pre | world != null
+     * @pre | position != null
+     * @pre | orientation != null
+     * 
+     * @post | this.getWorld() == world
+     * @post | this.getPosition().equals(position)
+     * @post | this.getOrientation().equals(orientation)
+     * @post | this.getMoveProbability() == Constants.SHELTER_MOVE_PROBABILITY
+     * @post | this.getInhabitants() != null && this.getInhabitants().isEmpty()
+     */
     Shelter(World world, Point position, Orientation orientation)
     {
         super(world, position, orientation, Constants.SHELTER_MOVE_PROBABILITY);
         this.inhabitants = new ArrayList<>();
     }
 
+    /**
+     * Checks whether this entity is of the prey type.
+     * 
+     * @return false since this entity is not a prey.
+     * 
+     * @post | result == false
+     */
     @Override
 	boolean isPreyPkg()
 	{
 	    return false;
 	}
 
+    /**
+     * Checks whether this entity is of the shelter type.
+     * 
+     * @return true since this entity is a shelter.
+     * 
+     * @post | result == true
+     */
 	@Override
 	boolean isShelterPkg()
 	{
-	    return false;
+	    return true;
 	}
 
+	/**
+     * Checks whether this entity is of the hunter type.
+     * 
+     * @return false since this entity is not a hunter.
+     * 
+     * @post | result == false
+     */
 	@Override
 	boolean isHunterPkg()
 	{
 	    return false;
 	}
 
-
-
+	/**
+     * Retrieves the list of inhabitants of this shelter.
+     * 
+     * @return The list of inhabitants.
+     * 
+     * @post | result != null
+     */
 	public ArrayList<Prey> getInhabitants()
     {
         return inhabitants;
     }
 
+	/**
+     * Performs actions for the shelter if it is alive. 
+     * This includes turning and moving.
+     * 
+     * @post If the shelter is alive, it may change its orientation and/or position.
+     */
     @Override
     public void performActionIfAlive()
     {
-
+    	if (!isDead()) 
+    	{
+            if (RandomUtil.unfairBool(Constants.SHELTER_TURN_PROBABILITY)) 
+            {
+                if (RandomUtil.bool()) 
+                {
+                    setOrientation(getOrientation().turnClockwise(1));
+                } 
+                else 
+                {
+                    setOrientation(getOrientation().turnCounterclockwise(1));
+                }
+            }
+            
+            if (RandomUtil.unfairBool(Constants.SHELTER_MOVE_PROBABILITY)) 
+            {
+                Vector movement = getOrientation().toVector();
+                Point newPosition = getPosition().move(movement);
+                if (world.isInside(newPosition) && world.isFree(newPosition)) 
+                {
+                    setPosition(newPosition);
+                }
+            }
+        }
     }
 
+    /**
+     * Retrieves the color associated with this shelter entity.
+     * 
+     * @return The color of this shelter entity (BLACK).
+     * 
+     * @post | result != null
+     */
     @Override
     public Color getColor()
     {
-        return null;
+    	return Color.BLACK;
     }
 
+    /**
+     * Returns a string representation of this shelter entity, including its position.
+     * 
+     * @return A string containing the position of this shelter entity.
+     * 
+     * @post | result != null
+     */
     @Override
     public String toString()
     {
@@ -71,7 +164,8 @@ public class Shelter extends MortalEntity
     /**
      * LEGIT
      */
-    public Shelter giveCopy() {
+    public Shelter giveCopy() 
+    {
     	return new Shelter(super.world, getPosition(), getOrientation());
     }
     
@@ -79,8 +173,11 @@ public class Shelter extends MortalEntity
      * Adds the specified prey to the list of inhabitants in the shelter.
      * 
      * @param prey The prey to add to the list of inhabitants.
+     * 
+     * @post | this.getInhabitants().contains(prey)
      */
-    public void addInhabitant(Prey prey) {
+    public void addInhabitant(Prey prey) 
+    {
         inhabitants.add(prey);
     }
 }
