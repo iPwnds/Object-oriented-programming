4c98b7d822b4bee05ecbf26c1eb223f48179c37a
Florian Braun <florianb.priv@gmail.com>
2024-05-13 01:18:50 +0200

    Final

---
+++ Effects/src/StubLogger.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class StubLogger extends Log {
+
+	private ArrayList<String> buffer = new ArrayList<>();
+	private ArrayList<Integer> inputs;
+	
+	public StubLogger(ArrayList<Integer> inputs) {
+		this.inputs = inputs;
+	}
+	
+	@Override
+	public void logMessage(String s) {
+		buffer.add(s);
+	}
+
+	public ArrayList<String> getBuffer() {
+		return new ArrayList<>(buffer);
+	}
+	
+	public int getInput() {
+		
+		// .size() is not working
+		
+		int inputs = inputs.get(inputs.size() - 1);
+		inputs.remove(inputs.size() - 1);
+		return inputs;
+	}
+	
+}
