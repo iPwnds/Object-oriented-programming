5d82304c7abe59da98c8dd71ba70b9f1db615d30
kasper lemaitre <kasper.lemaitre@student.kuleuven.be>
2024-05-23 23:43:11 +0200

    sheltertests

--- src/test/java/StudentTests/ShelterTest.java
+++ src/test/java/StudentTests/ShelterTest.java
@@ -8,10 +8,16 @@ import sim.Constants;
 import sim.entities.Prey;
 import sim.entities.Shelter;
 import sim.entities.World;
+import util.Color;
 import util.Orientation;
 import util.Point;
 
 public class ShelterTest {
-	
-
+	@Test
+	public void getColor() {
+		World world = new World(10,10);
+		Shelter shelter = world.createShelter(new Point(1,1), Orientation.createRandom());
+		assertEquals(shelter.getColor(), Color.BLACK);
 	}
+	
+}
