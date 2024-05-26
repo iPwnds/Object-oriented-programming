fcb08fd0f7aaba3544d640c3b99198e261ca323d
Florian Braun <florianb.priv@gmail.com>
2024-05-26 21:02:57 +0200

    option 2

--- Iterators/src/Iterators/ArrayList.java
+++ Iterators/src/Iterators/ArrayList.java
@@ -4,24 +4,26 @@ import java.util.Iterator;
 
 public class ArrayList implements Iterable<Object> {
 
-	public class ArrayListIterator implements Iterator<Object> {
-		
-		public int i = 0;
-		
-		@Override
-		public boolean hasNext() {
-			return i < ArrayList.this.elements.length;
-		}
-		
-		@Override
-		public Object next() {
-			return ArrayList.this.elements[i++];
-		}
 	
-	}
 	
 	@Override
 	public Iterator<Object> iterator() {
+		class ArrayListIterator implements Iterator<Object> {
+			
+			public int i = 0;
+			
+			@Override
+			public boolean hasNext() {
+				return i < ArrayList.this.elements.length;
+			}
+			
+			@Override
+			public Object next() {
+				return ArrayList.this.elements[i++];
+			}
+		
+		}
+		
 		return new ArrayListIterator();
 	}
 	
