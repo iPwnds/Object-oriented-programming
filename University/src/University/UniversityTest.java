18057350a91afebef2eb908b0b07beb0466ec67d
Florian Braun <florianb.priv@gmail.com>
2024-05-27 02:04:47 +0200

    init

---
+++ University/src/University/UniversityTest.java
@@ -0,0 +1,24 @@
+package University;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UniversityTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
