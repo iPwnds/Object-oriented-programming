710a72cc761f5da08eb94a9a7e7d360ab68fb82a
Florian Braun <florianb.priv@gmail.com>
2024-05-27 02:31:01 +0200

    midway

--- University/src/University/UniversityTest.java
+++ University/src/University/UniversityTest.java
@@ -1,24 +1,149 @@
 package University;
 
-import static org.junit.jupiter.api.Assertions.*;
+interface Iterator<T> {
+	boolean hasNext();
+	T next();
+}
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class UniversityTest {
-
-	@BeforeEach
-	void setUp() throws Exception {
+class LinkedList<T> {
+	class Node {
+		T element;
+		Node next;
+		
+		Node(T element, Node next) {
+			this.element = element;
+			this.next = next;
+		}
 	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	
+	Node first;
+	
+	void add(T element) {
+		if (first == null)
+			first = new Node(element, null);
+		else {
+			Node n = first;
+			while (n.next != null)
+				n = n.next;
+			n.next = new Node(element, null);
+		}
 	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	boolean contains(T element) {
+		for (Node n = first; n != null; n = n.next)
+			if (n.element == element)
+				return true;
+			return false;
 	}
+	
+	Iterator<T> iterator() {
+		return new Iterator<T>() {
+			Node n = first;
+			
+			@Override
+			public boolean hasNext() {
+				return n != null;
+			}
+			
+			@Override
+			public T next() {
+				Node n = this.n;
+				this.n = n.next;
+				return n.element;
+			}
+		};
+	}
+}
 
+class Student {
+	int nbCredits;
+}
+
+class StaffMember {
+	int nbPubs;
+}
+
+interface StudentIterator {
+	boolean hasNext();
+	Student next();
+}
+
+
+class LinkedListOfStudents {
+	class Node {
+		Student student;
+		Node next;
+		
+		Node(Student student, Node next) {
+			this.student = student;
+			this.next = next;
+		}
+	}
+	
+	Node first;
+	
+	void add(Student student) {
+		if (first == null)
+			first = new Node(student, null);
+		else {
+			Node n = first;
+			while (n.next != null)
+				n = n.next;
+			n.next = new Node(student, null);
+		}
+	}
+	
+	boolean contains(Student student) {
+		for (Node n = first; n != null; n = n.next)
+			if (n.student == student)
+				return true;
+			return false;
+	}
+	
+	StudentIterator iterator() {
+		return new StudentIterator() {
+			Node n = first;
+			
+			@Override
+			public boolean hasNext() {
+				return n != null;
+			}
+			
+			@Override
+			public Student next() {
+				Node n = this.n;
+				this.n = n.next;
+				return n.student;
+			}
+		};
+	}
+}
+
+class University {
+
+	LinkedList<Student> students = new LinkedList<Student>();
+	LinkedList<StaffMember> staffMembers = new LinkedList<StaffMember>();
+	
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+	
+	public void addStaffMember(StaffMember staffMember) {
+		staffMembers.add(staffMember);
+	}
+	
+	public int getTotalStudentNbCredits() {
+		int result = 0;
+		for (Iterator<Student> i = students.iterator(); i.hasNext(); )
+			result += i.next().nbCredits;
+		return result;
+	}
+	
+	public int getTotalNbPubs() {
+		int result = 0;
+		for (Iterator<StaffMember> i = staffMembers.iterator(); i.hasNext(); )
+			result += i.next().nbPubs;
+		return result;
+	}
+	
 }
