a4e1b8be41eddab6f9e0d9f093a2de66d5a73447
Florian Braun <florianb.priv@gmail.com>
2024-05-29 03:27:16 +0200

    realocation

--- CourseNotes/src/Single-object abstractions/FirstStepsInModularPt1/Interval.java
+++ CourseNotes/src/Single-object abstractions/FirstStepsInModularPt1/Interval.java
@@ -1,6 +1,10 @@
 package FirstStepsInModularPt1;
 
 class Interval {
+//	int lowerBound;
+//	int upperBound;
+	
+	
 	private int lowerBound;
 	private int upperBound;
 	
---
+++ CourseNotes/src/Single-object abstractions/ManagingComplexityThroughModularityAndAbstraction/File.java
@@ -0,0 +1,5 @@
+package ManagingComplexityThroughModularityAndAbstraction;
+
+public class File {
+
+}
